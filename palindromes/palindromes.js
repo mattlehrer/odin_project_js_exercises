4b3f366d7bcc4a2a18a7ab213b2c70ba0f5e1d62
Matt Lehrer <lehrerm@gmail.com>
2019-11-01 14:41:40 -0700

    palindromes passes all tests

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+  const arr = [...str.trim().toLowerCase()].filter(c => c.match(/[a-z]/));
+  for (let i = 0; i < arr.length / 2; i++) {
+    if (arr[i] !== arr[arr.length - i - 1]) return false;
+  }
+  return true;
 }
 
 module.exports = palindromes
