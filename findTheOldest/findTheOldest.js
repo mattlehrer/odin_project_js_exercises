253d54c6d21aaa588f16c4cf9fb448118cdd0eab
Matt Lehrer <lehrerm@gmail.com>
2019-11-01 14:33:53 -0700

    findTheOldest passes all tests

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+  return people.sort((a, b) => ((b.yearOfDeath || 2019) - b.yearOfBirth) - ((a.yearOfDeath || 2019) - a.yearOfBirth))[0]
 }
 
 module.exports = findTheOldest
