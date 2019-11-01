3bf308b581ad33c174ba2b02eb6e25f546a733ba
Matt Lehrer <lehrerm@gmail.com>
2019-11-01 14:27:41 -0700

    fibonacci passes all tests

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n = Number(n);
+  if (n < 1 || !Number.isInteger(n)) return "OOPS";
+  if (n < 3) return 1;
+  return fibonacci(n-2) + fibonacci(n-1);
 }
 
 module.exports = fibonacci
