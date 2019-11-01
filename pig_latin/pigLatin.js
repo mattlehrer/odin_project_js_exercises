f05e834a4c338575124d95a822aaf88f5aba855e
Matt Lehrer <lehrerm@gmail.com>
2019-11-01 14:55:12 -0700

    pigLatin passes all tests

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,11 @@
-function translate() {
-	// body...
+function translate(str) {
+  return str.split(' ').map(w => {
+    let vowel = w.match(/[aeiou]/)[0];
+    if (vowel === 'u' && w.indexOf(vowel) > 0 && w[w.indexOf(vowel) - 1] === 'q') {
+      vowel = w[w.indexOf(vowel) + 1];
+    }
+    return `${w.slice(w.indexOf(vowel),w.length)}${w.slice(0, w.indexOf(vowel))}ay`;
+  }).join(' ');
 }
 
 
