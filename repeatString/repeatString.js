d85b12580603e9c76677253a499cb034be6a4235
Matt Lehrer <lehrerm@gmail.com>
2019-11-01 12:19:09 -0700

    repeatString passes all tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+  if (times < 0) return 'ERROR';
+  return new Array(times).fill(str, 0, times).join('');
 }
 
 module.exports = repeatString
