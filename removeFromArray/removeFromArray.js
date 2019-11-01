73aad63de9639d20528fe26520d9a99be3275a2c
Matt Lehrer <lehrerm@gmail.com>
2019-11-01 12:33:58 -0700

    removeFromArray passes all tests

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+  toRemove.map(x => {
+    if (arr.indexOf(x) > -1) arr.splice(arr.indexOf(x), 1)
+  });
+  return arr;
 }
 
 module.exports = removeFromArray
