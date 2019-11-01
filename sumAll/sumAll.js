4fb527094f8408cf9bdf6a9e15b977ba74010d60
Matt Lehrer <lehrerm@gmail.com>
2019-11-01 12:42:49 -0700

    sumAll passes all tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  if (start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number') {
+    return 'ERROR';
+  }
+  
+  const a = Math.min(start, end);
+  const b = Math.max(start, end);
+  return (b * (b + 1) / 2) - ((a - 1) * a / 2);
 }
 
 module.exports = sumAll
