f8e78ddba7fc3a43fb4c0b35839a502c175cb0b9
Matt Lehrer <lehrerm@gmail.com>
2019-11-01 12:54:22 -0700

    tempConversion passes all tests

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const precise = (temp - 32) * 5 / 9;
+  const str = String(precise);
+  if (str.includes('.')) {
+    return Number((precise).toFixed(1));
+  }
+  return precise;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const precise = temp * 9 / 5 + 32;
+  const str = String(precise);
+  if (str.includes('.')) {
+    return Number((precise).toFixed(1));
+  }
+  return precise;
 }
 
 module.exports = {
