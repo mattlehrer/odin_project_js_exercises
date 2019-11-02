371c68c1cf5f1e58c34c95a80c9b91c56c92807f
Matt Lehrer <lehrerm@gmail.com>
2019-11-01 21:03:17 -0700

    snakeCase passes all tests

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,23 @@
-const snakeCase = function() {
-
+const snakeCase = function(str) {
+  if (str.match(/[A-Z]/g) && str.match(/[A-Z]/g).length > str.match(/[a-z]/g).length) {
+    return [...str
+      .match(/[a-z].*[a-z]/i)[0] // trim spaces and punctuation
+      .replace(/[,\.-]/g, ' ')
+      .trim()
+      .replace(/ +/g, ' ')
+      .split(' ')]
+      .join('_')
+      .toLowerCase();
+  }
+  return [...str
+    .match(/[a-z].*[a-z]/i)[0] // trim spaces and punctuation
+    .replace(/[,\.-]/g, ' ')
+    .replace(/[A-Z]/g, ' $&')
+    .trim()
+    .replace(/ +/g, ' ')
+    .split(' ')]
+    .join('_')
+    .toLowerCase();
 }
 
 module.exports = snakeCase
